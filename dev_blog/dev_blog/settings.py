77be765edb5e9b13a80fbc2ea92e3701172f78dd
JordyV23 <jordyvargas0123@gmail.com>
2023-09-15 07:45:26 -0600

    Cambios realizados: -Instalacion de dependencias: pyscopg2 -*- Pillow -*- python-decouple -Configuracion de archivo .env -Conexion a la base de datos en POSTGRES -Creacion de .env.example para ejemplo -Creacion de modelos: User -*- Profile

--- dev_blog/dev_blog/settings.py
+++ dev_blog/dev_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +80,16 @@ WSGI_APPLICATION = 'dev_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT', cast=int)
     }
 }
+#                  aplicacion.modelo
+AUTH_USER_MODEL = 'user.User'
 
 
 # Password validation
